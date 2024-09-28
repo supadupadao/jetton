83947d5919afee6475d870241ede01ac8e9d0368
ya7on <hello@syoma.website>
2024-09-28 15:46:37 +0300

    feat: initial

---
+++ tests/JettonMaster.spec.ts
@@ -0,0 +1,44 @@
+import { Blockchain, SandboxContract, TreasuryContract } from '@ton/sandbox';
+import { toNano } from '@ton/core';
+import { JettonWallet } from '../build/Jetton/tact_JettonWallet';
+import { JettonMaster } from '../build/Jetton/tact_JettonMaster';
+import '@ton/test-utils';
+
+describe('JettonMaster', () => {
+    let blockchain: Blockchain;
+    let deployer: SandboxContract<TreasuryContract>;
+    let jettonMaster: SandboxContract<JettonMaster>;
+    let jettonWallet: SandboxContract<JettonWallet>;
+
+    beforeEach(async () => {
+        blockchain = await Blockchain.create();
+
+        deployer = await blockchain.treasury('deployer');
+
+        jettonMaster = blockchain.openContract(await JettonMaster.fromInit(deployer.address));
+        jettonWallet = blockchain.openContract(await JettonWallet.fromInit(jettonMaster.address, deployer.address));
+
+        const deployResult = await jettonMaster.send(
+            deployer.getSender(),
+            {
+                value: toNano("0.05"),
+            },
+            {
+                $$type: 'Deploy',
+                queryId: 0n,
+            }
+        );
+
+        expect(deployResult.transactions).toHaveTransaction({
+            from: deployer.address,
+            to: jettonMaster.address,
+            success: true,
+            deploy: true,
+        });
+    });
+
+    it('should correct build wallet address', async () => {
+        let walletAddressData = await jettonMaster.getGetWalletAddress(deployer.address);
+        expect(walletAddressData.toString()).toEqual(jettonWallet.address.toString());
+    });
+});
